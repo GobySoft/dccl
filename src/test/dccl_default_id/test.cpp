af040edce270028acfc10a70a96c928b2278395d
Toby Schneider <toby@gobysoft.org>
2016-05-13 17:46:27 -0400

    Updated header preamble

--- src/test/dccl_default_id/test.cpp
+++ src/test/dccl_default_id/test.cpp
@@ -1,26 +1,3 @@
-// Copyright 2009-2014 Toby Schneider (https://launchpad.net/~tes)
-//                     GobySoft, LLC (2013-)
-//                     Massachusetts Institute of Technology (2007-2014)
-//                     DCCL Developers Team (https://launchpad.net/~dccl-dev)
-//
-// This file is part of the Dynamic Compact Control Language Applications
-// ("DCCL").
-//
-// DCCL is free software: you can redistribute them and/or modify
-// them under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// DCCL is distributed in the hope that they will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with DCCL.  If not, see <http://www.gnu.org/licenses/>.
-
-
-
 // tests fixed id header
 
 #include "dccl/codec.h"
--- src/test/dccl_v2_header/test.cpp
+++ src/test/dccl_v2_header/test.cpp
@@ -1,27 +1,3 @@
-// Copyright 2009-2013 Toby Schneider (https://launchpad.net/~tes)
-//                     Massachusetts Institute of Technology (2007-)
-//                     Woods Hole Oceanographic Institution (2007-)
-//                     DCCL Developers Team (https://launchpad.net/~dccl-dev)
-// 
-//
-// This file is part of the Dynamic Compact Control Language Library
-// ("DCCL").
-//
-// DCCL is free software: you can redistribute them and/or modify
-// them under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// DCCL is distributed in the hope that they will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with DCCL.  If not, see <http://www.gnu.org/licenses/>.
-
-
-
 // tests proper encoding of standard Goby header
 
 #include "dccl/codec.h"
