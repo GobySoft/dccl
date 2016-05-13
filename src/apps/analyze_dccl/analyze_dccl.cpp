3130a29e4f97d7464bb30f9356c94e551af4d406
Toby Schneider <toby@gobysoft.org>
2016-05-13 18:02:45 -0400

    Added new GS only header for new files

--- src/apps/analyze_dccl/analyze_dccl.cpp
+++ src/apps/analyze_dccl/analyze_dccl.cpp
@@ -1,3 +1,24 @@
+// Copyright 2009-2016 Toby Schneider (http://gobysoft.org/index.wt/people/toby)
+//                     GobySoft, LLC (for 2013-)
+//                     Massachusetts Institute of Technology (for 2007-2014)
+//                     Community contributors (see AUTHORS file)
+//
+//
+// This file is part of the Dynamic Compact Control Language Library
+// ("DCCL").
+//
+// DCCL is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// DCCL is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with DCCL.  If not, see <http://www.gnu.org/licenses/>.
 #include <google/protobuf/descriptor.h>
 
 #include "dccl/codec.h"
