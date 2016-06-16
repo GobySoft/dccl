66bf38f71bd70989618f4b933aab5c7850e0ff1d
Toby Schneider <toby@gobysoft.org>
2016-06-16 14:47:16 -0400

    Added (dccl.msg).unit_system option for setting unit system default for all fields in a message. Made it a requirement that all fields have a defined system by using (dccl.msg).unit_system and/or (dccl.field).units.system. Updated plugin and related unit test to match.

--- src/apps/pb_plugin/pb_plugin.cpp
+++ src/apps/pb_plugin/pb_plugin.cpp
@@ -48,8 +48,12 @@ class DCCLGenerator : public google::protobuf::compiler::CodeGenerator {
                   google::protobuf::compiler::GeneratorContext* generator_context,
                   std::string* error) const;
  private:
-    void generate_message(const google::protobuf::Descriptor* desc, google::protobuf::compiler::GeneratorContext* generator_context) const;
-    void generate_field(const google::protobuf::FieldDescriptor* field, google::protobuf::io::Printer* printer) const;
+    void generate_message(const google::protobuf::Descriptor* desc,
+                          google::protobuf::compiler::GeneratorContext* generator_context,
+                          boost::shared_ptr<std::string> message_unit_system = boost::shared_ptr<std::string>()) const;
+    void generate_field(const google::protobuf::FieldDescriptor* field,
+                        google::protobuf::io::Printer* printer,
+                        boost::shared_ptr<std::string> message_unit_system) const;
     bool check_field_type(const google::protobuf::FieldDescriptor* field) const;
     
 };
@@ -87,7 +91,14 @@ bool DCCLGenerator::Generate(const google::protobuf::FileDescriptor* file,
         
         for(int message_i = 0, message_n = file->message_type_count(); message_i < message_n; ++message_i)
         {
-            generate_message(file->message_type(message_i), generator_context);            
+            try
+            {
+                generate_message(file->message_type(message_i), generator_context);
+            }
+            catch(std::exception& e)
+            {
+                throw(std::runtime_error(std::string("Failed to generate DCCL code: \n") + e.what()));
+            }
         }
         
         boost::shared_ptr<google::protobuf::io::ZeroCopyOutputStream> include_output(
@@ -115,106 +126,138 @@ bool DCCLGenerator::Generate(const google::protobuf::FileDescriptor* file,
 }
 
 
-void DCCLGenerator::generate_message(const google::protobuf::Descriptor* desc, google::protobuf::compiler::GeneratorContext* generator_context) const
+void DCCLGenerator::generate_message(const google::protobuf::Descriptor* desc, google::protobuf::compiler::GeneratorContext* generator_context, boost::shared_ptr<std::string> message_unit_system) const
 {
-    boost::shared_ptr<google::protobuf::io::ZeroCopyOutputStream> output(
-        generator_context->OpenForInsert(filename_h_, "class_scope:" + desc->full_name()));
-    google::protobuf::io::Printer printer(output.get(), '$');
-
-    if(desc->options().HasExtension(dccl::msg))
+    try
     {
-        std::stringstream id_enum;
-        id_enum << "enum DCCLParameters { DCCL_ID = " << desc->options().GetExtension(dccl::msg).id() << ", " <<
-            " DCCL_MAX_BYTES = " << desc->options().GetExtension(dccl::msg).max_bytes() << " };";
-        printer.Print(id_enum.str().c_str());
-    }
-    
-        
-    
-    for(int field_i = 0, field_n = desc->field_count(); field_i < field_n; ++field_i)
-    {
-        generate_field(desc->field(field_i), &printer);
-    }
+        boost::shared_ptr<google::protobuf::io::ZeroCopyOutputStream> output(
+            generator_context->OpenForInsert(filename_h_, "class_scope:" + desc->full_name()));
+        google::protobuf::io::Printer printer(output.get(), '$');
 
-    for(int nested_type_i = 0, nested_type_n = desc->nested_type_count(); nested_type_i < nested_type_n; ++nested_type_i)
-        generate_message(desc->nested_type(nested_type_i), generator_context);
+        if(desc->options().HasExtension(dccl::msg))
+        {
+            std::stringstream id_enum;
+            id_enum << "enum DCCLParameters { DCCL_ID = " << desc->options().GetExtension(dccl::msg).id() << ", " <<
+                " DCCL_MAX_BYTES = " << desc->options().GetExtension(dccl::msg).max_bytes() << " };";
+            printer.Print(id_enum.str().c_str());
+
+
+            // set message level unit system - used if fields do not specify
+            const dccl::DCCLMessageOptions& dccl_msg_options = desc->options().GetExtension(dccl::msg);
+            if(dccl_msg_options.has_unit_system())
+            {
+                message_unit_system.reset(new std::string(dccl_msg_options.unit_system()));
+                systems_to_include_.insert(dccl_msg_options.unit_system());
+            }
+        }    
+    
+        for(int field_i = 0, field_n = desc->field_count(); field_i < field_n; ++field_i)
+        {
+            generate_field(desc->field(field_i), &printer, message_unit_system);
+        }
+
+        for(int nested_type_i = 0, nested_type_n = desc->nested_type_count(); nested_type_i < nested_type_n; ++nested_type_i)
+            generate_message(desc->nested_type(nested_type_i), generator_context, message_unit_system);
+    }
+    catch(std::exception& e)
+    {
+        throw(std::runtime_error(std::string("Message: \n" + desc->full_name() + "\n" + e.what())));
+    }
 }
 
-void DCCLGenerator::generate_field(const google::protobuf::FieldDescriptor* field, google::protobuf::io::Printer* printer) const
+void DCCLGenerator::generate_field(const google::protobuf::FieldDescriptor* field, google::protobuf::io::Printer* printer, boost::shared_ptr<std::string> message_unit_system) const
 {
-    const dccl::DCCLFieldOptions& dccl_options = field->options().GetExtension(dccl::field);
+    try
+    {
+        const dccl::DCCLFieldOptions& dccl_field_options = field->options().GetExtension(dccl::field);
 
-    if(!dccl_options.has_units()) {return;}
+        if(!dccl_field_options.has_units()) {return;}
    
-    if((dccl_options.units().has_base_dimensions() && dccl_options.units().has_derived_dimensions())||
-       (dccl_options.units().has_base_dimensions() && dccl_options.units().has_unit()) ||
-       (dccl_options.units().has_unit() && dccl_options.units().has_derived_dimensions()))
-    {
-        throw(std::runtime_error("May define either (dccl.field).units.base_dimensions or (dccl.field).units.derived_dimensions or (dccl.field).units.unit, but not more than one."));
-    }
-    else if(dccl_options.units().has_unit())
-    {
-        std::stringstream new_methods;
+        if((dccl_field_options.units().has_base_dimensions() && dccl_field_options.units().has_derived_dimensions())||
+           (dccl_field_options.units().has_base_dimensions() && dccl_field_options.units().has_unit()) ||
+           (dccl_field_options.units().has_unit() && dccl_field_options.units().has_derived_dimensions()))
+        {
+            throw(std::runtime_error("May define either (dccl.field).units.base_dimensions or (dccl.field).units.derived_dimensions or (dccl.field).units.unit, but not more than one."));
+        }
+        else if(dccl_field_options.units().has_unit())
+        {
+            std::stringstream new_methods;
 
-	construct_units_typedef_from_base_unit(field->name(), dccl_options.units().unit(), dccl_options.units().relative_temperature(), new_methods);
-	construct_field_class_plugin(field->name(),
-                                 new_methods, 
-                                 dccl::units::get_field_type_name(field->cpp_type()),
-                                 field->is_repeated());
-	printer->Print(new_methods.str().c_str());
-        base_units_to_include_.insert(dccl_options.units().unit());
-    }
-    else if(dccl_options.units().has_base_dimensions())
-    {
-
-        std::stringstream new_methods;
-                
-        std::vector<double> powers;
-        std::vector<std::string> short_dimensions;
-        std::vector<std::string> dimensions;
-        if(dccl::units::parse_base_dimensions(dccl_options.units().base_dimensions().begin(),
-                                         dccl_options.units().base_dimensions().end(),
-					      powers, short_dimensions, dimensions))
-	  {
-	    construct_base_dims_typedef(dimensions, powers, field->name(), dccl_options.units().system(), dccl_options.units().relative_temperature(), new_methods);
-
-            bool is_integer = check_field_type(field);                    
+            construct_units_typedef_from_base_unit(field->name(), dccl_field_options.units().unit(), dccl_field_options.units().relative_temperature(), new_methods);
             construct_field_class_plugin(field->name(),
                                          new_methods, 
                                          dccl::units::get_field_type_name(field->cpp_type()),
                                          field->is_repeated());
             printer->Print(new_methods.str().c_str());
-            systems_to_include_.insert(dccl_options.units().system());
-	  }
-        else
-        {
-            throw(std::runtime_error(std::string("Failed to parse base_dimensions string: \"" + dccl_options.units().base_dimensions() + "\"")));
+            base_units_to_include_.insert(dccl_field_options.units().unit());
         }
-    }
-    else if(dccl_options.units().has_derived_dimensions())   
-    {
-        std::stringstream new_methods;
+        else if(dccl_field_options.units().has_base_dimensions())
+        {
+
+            std::stringstream new_methods;
                 
-        std::vector<std::string> operators;
-        std::vector<std::string> dimensions;
-        if(dccl::units::parse_derived_dimensions(dccl_options.units().derived_dimensions().begin(),
-						 dccl_options.units().derived_dimensions().end(),
-						 operators, dimensions))
-	  {
-            construct_derived_dims_typedef(dimensions, operators, field->name(), dccl_options.units().system(), dccl_options.units().relative_temperature(), new_methods);
+            std::vector<double> powers;
+            std::vector<std::string> short_dimensions;
+            std::vector<std::string> dimensions;
+            if(dccl::units::parse_base_dimensions(dccl_field_options.units().base_dimensions().begin(),
+                                                  dccl_field_options.units().base_dimensions().end(),
+                                                  powers, short_dimensions, dimensions))
+            {
+                if(!dccl_field_options.units().has_system() && !message_unit_system)
+                    throw(std::runtime_error(std::string("Field must have 'system' defined or message must have 'unit_system' defined when using 'base_dimensions'.")));
+              
+                // default to system set in the field, otherwise use the system set at the message level
+                const std::string& unit_system = (!dccl_field_options.units().has_system() && message_unit_system) ? *message_unit_system : dccl_field_options.units().system();
+              
+                construct_base_dims_typedef(dimensions, powers, field->name(), unit_system, dccl_field_options.units().relative_temperature(), new_methods);
+
+                bool is_integer = check_field_type(field);                    
+                construct_field_class_plugin(field->name(),
+                                             new_methods, 
+                                             dccl::units::get_field_type_name(field->cpp_type()),
+                                             field->is_repeated());
+                printer->Print(new_methods.str().c_str());
+                systems_to_include_.insert(unit_system);
+            }
+            else
+            {
+                throw(std::runtime_error(std::string("Failed to parse base_dimensions string: \"" + dccl_field_options.units().base_dimensions() + "\"")));
+            }
+        }
+        else if(dccl_field_options.units().has_derived_dimensions())   
+        {
+            std::stringstream new_methods;
+                
+            std::vector<std::string> operators;
+            std::vector<std::string> dimensions;
+            if(dccl::units::parse_derived_dimensions(dccl_field_options.units().derived_dimensions().begin(),
+                                                     dccl_field_options.units().derived_dimensions().end(),
+                                                     operators, dimensions))
+            {
+                if(!dccl_field_options.units().has_system() && !message_unit_system)
+                    throw(std::runtime_error(std::string("Field must have 'system' defined or message must have 'unit_system' defined when using 'derived_dimensions'.")));
+                const std::string& unit_system = (!dccl_field_options.units().has_system() && message_unit_system) ? *message_unit_system : dccl_field_options.units().system();
+  
+                construct_derived_dims_typedef(dimensions, operators, field->name(), unit_system, dccl_field_options.units().relative_temperature(), new_methods);
                         
-            bool is_integer = check_field_type(field);
-            construct_field_class_plugin(field->name(),
-                                         new_methods, 
-                                         dccl::units::get_field_type_name(field->cpp_type()),
-                                         field->is_repeated());
-            printer->Print(new_methods.str().c_str());
-            systems_to_include_.insert(dccl_options.units().system());
+                bool is_integer = check_field_type(field);
+                construct_field_class_plugin(field->name(),
+                                             new_methods, 
+                                             dccl::units::get_field_type_name(field->cpp_type()),
+                                             field->is_repeated());
+                printer->Print(new_methods.str().c_str());
+                systems_to_include_.insert(unit_system);
+            }
+            else
+            {
+                throw(std::runtime_error(std::string("Failed to parse derived_dimensions string: \"" + dccl_field_options.units().derived_dimensions() + "\"")));
+            }
         }
-        else
-        {
-            throw(std::runtime_error(std::string("Failed to parse base_dimensions string: \"" + dccl_options.units().base_dimensions() + "\"")));
-        }
+        
+    }
+    catch(std::exception& e)
+    {
+        throw(std::runtime_error(std::string("Field: \n" + field->DebugString() + "\n" + e.what())));
     }
 }
 
