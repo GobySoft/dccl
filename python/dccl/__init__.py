d7e83dc169670a0d98b28a2991868faabd43abc9
Chris Murphy <cmurphy@aphysci.com>
2018-05-08 14:43:04 -0400

    Adding python binding - can encode() and id().

---
+++ python/dccl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module provides Python bindings for the Dynamic Compact Control Language (DCCL) library."""
+__author__ = "Chris Murphy, Toby Schneider"
+__copyright__ = "Copyright 2018, The DCCL Project"
+__license__ = "LGPL"
+__version__ = "3.0.0a1"
+
+from _dccl import *
+
---
+++ python/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Setuptools build script for DCCL Bindings."""
+__author__ = "Chris Murphy, Toby Schneider"
+__copyright__ = "Copyright 2018, The DCCL Project"
+__license__ = "LGPL"
+__version__ = "3.0.0a1"
+
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+
+setup(
+    name="dccl",
+    version=__version__,
+    description="Python Bindings for DCCL.",
+    author="Chris Murphy",
+    author_email="cmurphy@aphysci.com",
+
+    packages=find_packages(),
+    ext_modules = [
+        Extension(
+            "dccl._dccl",
+            ["dccl/_dccl.cc"],
+            libraries=['dccl', 'protobuf'],
+#           include_dirs=['/some/path/to/include/'],
+#           library_dirs=['/some/path/to/include/'],
+            extra_compile_args = ["-Wno-write-strings"], # Hide a bunch of c++ warnings.
+        )
+    ]
+)
+
