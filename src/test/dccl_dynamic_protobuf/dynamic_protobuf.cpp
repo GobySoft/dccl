26c454b440ffa278b2129b04809ca11088036805
Toby Schneider <toby@gobysoft.org>
2019-05-21 18:29:12 +0100

    Remove C++11-isms

--- src/test/dccl_dynamic_protobuf/dynamic_protobuf.cpp
+++ src/test/dccl_dynamic_protobuf/dynamic_protobuf.cpp
@@ -29,85 +29,84 @@
 
 #include "test_a.pb.h"
 
-int main(int argc, char* argv[])
-{
-    if(argc != 2)
-    {
-        std::cerr << "Usage: " << argv[0] << " /path/to/libtest_dyn_protobuf" << std::endl;
-        exit(1);
-    }
-    
-    void* dl_handle = dlopen(argv[1], RTLD_LAZY);
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " /path/to/libtest_dyn_protobuf"
+              << std::endl;
+    exit(1);
+  }
 
-    if (!dl_handle)
-    {
-        std::cerr << "Failed to open libtest_dyn_protobuf" SHARED_LIBRARY_SUFFIX << ", error: " << dlerror() << std::endl;
-        exit(1);
-    }
+  void *dl_handle = dlopen(argv[1], RTLD_LAZY);
 
-    boost::shared_ptr<google::protobuf::SimpleDescriptorDatabase> simple_database(
-        new google::protobuf::SimpleDescriptorDatabase);
-    dccl::DynamicProtobufManager::add_database(simple_database);
+  if (!dl_handle) {
+    std::cerr << "Failed to open libtest_dyn_protobuf" SHARED_LIBRARY_SUFFIX
+              << ", error: " << dlerror() << std::endl;
+    exit(1);
+  }
 
-    {
-        // testing compiled in
-        auto adyn_msg = dccl::DynamicProtobufManager::new_protobuf_message<
-            std::shared_ptr<google::protobuf::Message> >("A");
+  boost::shared_ptr<google::protobuf::SimpleDescriptorDatabase> simple_database(
+      new google::protobuf::SimpleDescriptorDatabase);
+  dccl::DynamicProtobufManager::add_database(simple_database);
 
-        std::cout << adyn_msg->GetDescriptor()->DebugString() << std::endl;
+  {
+    // testing compiled in
+    boost::shared_ptr<google::protobuf::Message> adyn_msg =
+        dccl::DynamicProtobufManager::new_protobuf_message("A");
 
-        // testing dlopen'd
-        auto bdyn_msg = dccl::DynamicProtobufManager::new_protobuf_message<
-            std::shared_ptr<google::protobuf::Message> >("B");
+    std::cout << adyn_msg->GetDescriptor()->DebugString() << std::endl;
 
-        std::cout << bdyn_msg->GetDescriptor()->DebugString() << std::endl;
+    // testing dlopen'd
+    boost::shared_ptr<google::protobuf::Message> bdyn_msg =
+        dccl::DynamicProtobufManager::new_protobuf_message("B");
 
-        // test non-existent
-        try
-        {
-            auto cdyn_msg = dccl::DynamicProtobufManager::new_protobuf_message<
-                std::shared_ptr<google::protobuf::Message> >("C");
-            // should throw
-            assert(false);
-        }
-        catch (std::exception& e)
-        {
-            // expected
-        }
+    std::cout << bdyn_msg->GetDescriptor()->DebugString() << std::endl;
 
-        // test dynamically loaded
-        google::protobuf::FileDescriptorProto d_proto;
-        std::string d_proto_str = "name: \"goby/test/util/dynamic_protobuf/test_d.proto\" "
-                                  "message_type {   name: \"D\"   field {     name: \"d1\"     "
-                                  "number: 1     label: LABEL_REQUIRED     type: TYPE_DOUBLE  } } ";
-
-        google::protobuf::TextFormat::ParseFromString(d_proto_str, &d_proto);
-        dccl::DynamicProtobufManager::add_protobuf_file(d_proto);
-
-        auto ddyn_msg = dccl::DynamicProtobufManager::new_protobuf_message<
-            std::shared_ptr<google::protobuf::Message> >("D");
-
-        std::cout << ddyn_msg->GetDescriptor()->DebugString() << std::endl;
-
-        // test dynamically via separate database
-        google::protobuf::FileDescriptorProto e_proto;
-        std::string e_proto_str = "name: \"goby/test/util/dynamic_protobuf/test_e.proto\" "
-                                  "message_type {   name: \"E\"   field {     name: \"e1\"     "
-                                  "number: 1     label: LABEL_REQUIRED     type: TYPE_DOUBLE  } } ";
-
-        google::protobuf::TextFormat::ParseFromString(e_proto_str, &e_proto);
-
-        simple_database->Add(e_proto);
-
-        auto edyn_msg = dccl::DynamicProtobufManager::new_protobuf_message<
-            std::shared_ptr<google::protobuf::Message> >("E");
-        std::cout << edyn_msg->GetDescriptor()->DebugString() << std::endl;
-
-        std::cout << "all tests passed" << std::endl;
+    // test non-existent
+    try {
+      boost::shared_ptr<google::protobuf::Message> cdyn_msg =
+          dccl::DynamicProtobufManager::new_protobuf_message<
+              boost::shared_ptr<google::protobuf::Message>>("C");
+      // should throw
+      assert(false);
+    } catch (std::exception &e) {
+      // expected
     }
 
-    dccl::DynamicProtobufManager::protobuf_shutdown();
+    // test dynamically loaded
+    google::protobuf::FileDescriptorProto d_proto;
+    std::string d_proto_str =
+        "name: \"goby/test/util/dynamic_protobuf/test_d.proto\" "
+        "message_type {   name: \"D\"   field {     name: \"d1\"     "
+        "number: 1     label: LABEL_REQUIRED     type: TYPE_DOUBLE  } } ";
 
-    dlclose(dl_handle);
-    return 0;
+    google::protobuf::TextFormat::ParseFromString(d_proto_str, &d_proto);
+    dccl::DynamicProtobufManager::add_protobuf_file(d_proto);
+
+    boost::shared_ptr<google::protobuf::Message> ddyn_msg =
+        dccl::DynamicProtobufManager::new_protobuf_message("D");
+
+    std::cout << ddyn_msg->GetDescriptor()->DebugString() << std::endl;
+
+    // test dynamically via separate database
+    google::protobuf::FileDescriptorProto e_proto;
+    std::string e_proto_str =
+        "name: \"goby/test/util/dynamic_protobuf/test_e.proto\" "
+        "message_type {   name: \"E\"   field {     name: \"e1\"     "
+        "number: 1     label: LABEL_REQUIRED     type: TYPE_DOUBLE  } } ";
+
+    google::protobuf::TextFormat::ParseFromString(e_proto_str, &e_proto);
+
+    simple_database->Add(e_proto);
+
+    boost::shared_ptr<google::protobuf::Message> edyn_msg =
+        dccl::DynamicProtobufManager::new_protobuf_message("E");
+    std::cout << edyn_msg->GetDescriptor()->DebugString() << std::endl;
+
+    std::cout << "all tests passed" << std::endl;
+  }
+
+  dccl::DynamicProtobufManager::protobuf_shutdown();
+
+  dlclose(dl_handle);
+  return 0;
 }
