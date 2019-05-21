3f3b6826c05a6b763423b54c35445057fccfbd06
Toby Schneider <toby@gobysoft.org>
2019-05-21 16:22:56 +0100

    Moved dynamic protobuf test from Goby

---
+++ src/test/dccl_dynamic_protobuf/dynamic_protobuf.cpp
@@ -0,0 +1,107 @@
+// Copyright 2009-2017 Toby Schneider (http://gobysoft.org/index.wt/people/toby)
+//                     GobySoft, LLC (for 2013-)
+//                     Massachusetts Institute of Technology (for 2007-2014)
+//                     Community contributors (see AUTHORS file)
+//
+//
+// This file is part of the Dynamic Compact Control Language Library
+// ("DCCL").
+//
+// DCCL is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// DCCL is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with DCCL.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "dccl/dynamic_protobuf_manager.h"
+#include <cassert>
+#include <dlfcn.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/text_format.h>
+#include <iostream>
+
+#include "test_a.pb.h"
+
+int main()
+{
+    void* dl_handle = dlopen("libtest_dyn_protobuf" SHARED_LIBRARY_SUFFIX, RTLD_LAZY);
+
+    if (!dl_handle)
+    {
+        std::cerr << "Failed to open libtest_dyn_protobuf" SHARED_LIBRARY_SUFFIX << std::endl;
+        exit(1);
+    }
+
+    boost::shared_ptr<google::protobuf::SimpleDescriptorDatabase> simple_database(
+        new google::protobuf::SimpleDescriptorDatabase);
+    dccl::DynamicProtobufManager::add_database(simple_database);
+
+    {
+        // testing compiled in
+        auto adyn_msg = dccl::DynamicProtobufManager::new_protobuf_message<
+            std::shared_ptr<google::protobuf::Message> >("A");
+
+        std::cout << adyn_msg->GetDescriptor()->DebugString() << std::endl;
+
+        // testing dlopen'd
+        auto bdyn_msg = dccl::DynamicProtobufManager::new_protobuf_message<
+            std::shared_ptr<google::protobuf::Message> >("B");
+
+        std::cout << bdyn_msg->GetDescriptor()->DebugString() << std::endl;
+
+        // test non-existent
+        try
+        {
+            auto cdyn_msg = dccl::DynamicProtobufManager::new_protobuf_message<
+                std::shared_ptr<google::protobuf::Message> >("C");
+            // should throw
+            assert(false);
+        }
+        catch (std::exception& e)
+        {
+            // expected
+        }
+
+        // test dynamically loaded
+        google::protobuf::FileDescriptorProto d_proto;
+        std::string d_proto_str = "name: \"goby/test/util/dynamic_protobuf/test_d.proto\" "
+                                  "message_type {   name: \"D\"   field {     name: \"d1\"     "
+                                  "number: 1     label: LABEL_REQUIRED     type: TYPE_DOUBLE  } } ";
+
+        google::protobuf::TextFormat::ParseFromString(d_proto_str, &d_proto);
+        dccl::DynamicProtobufManager::add_protobuf_file(d_proto);
+
+        auto ddyn_msg = dccl::DynamicProtobufManager::new_protobuf_message<
+            std::shared_ptr<google::protobuf::Message> >("D");
+
+        std::cout << ddyn_msg->GetDescriptor()->DebugString() << std::endl;
+
+        // test dynamically via separate database
+        google::protobuf::FileDescriptorProto e_proto;
+        std::string e_proto_str = "name: \"goby/test/util/dynamic_protobuf/test_e.proto\" "
+                                  "message_type {   name: \"E\"   field {     name: \"e1\"     "
+                                  "number: 1     label: LABEL_REQUIRED     type: TYPE_DOUBLE  } } ";
+
+        google::protobuf::TextFormat::ParseFromString(e_proto_str, &e_proto);
+
+        simple_database->Add(e_proto);
+
+        auto edyn_msg = dccl::DynamicProtobufManager::new_protobuf_message<
+            std::shared_ptr<google::protobuf::Message> >("E");
+        std::cout << edyn_msg->GetDescriptor()->DebugString() << std::endl;
+
+        std::cout << "all tests passed" << std::endl;
+    }
+
+    dccl::DynamicProtobufManager::protobuf_shutdown();
+
+    dlclose(dl_handle);
+    return 0;
+}
