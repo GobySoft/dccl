2c788ccebf5e665ba9d1036aee381e51b3cb4798
Toby Schneider <toby@gobysoft.org>
2023-10-20 00:12:13 -0400

    Finished cleaning up default codec load

--- src/codecs3/default_field_codec_impl.h
+++ src/codecs3/default_field_codec_impl.h
@@ -1,4 +1,8 @@
 #define CODEC_VERSION 3
 #define CODEC_VERSION_NAMESPACE v3
 
+#include "field_codec_default.h"
+#include "field_codec_default_message.h"
+#include "field_codec_presence.h"
+
 #include "../default_field_codec.h"
